114faadceb7c564393050c9b84aa9f3bc3e56e6c
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-20 00:18:06 +0000

    typo corrected

--- task_01.py
+++ task_01.py
@@ -8,22 +8,22 @@ ACCENTQ = 'Which accent color, '
 HIGHLIGHTQ = 'Which highlight color, '
 MYERROR = 'Sorry, the colors you chose is not valid, please try again.'
 
-BASE = raw_input(BASEQ + '"Seattle Gray" or "Manatee"? ')
+BASE = raw_input(BASEQ + '"Seattle Gray" or "Manatee"?: ')
 
 if BASE == 'Seattle Gray':
-    ACCENT = raw_input(ACCENTQ + '"Ceramic Glaze" or "Cumulus Nimbus"? ')
+    ACCENT = raw_input(ACCENTQ + '"Ceramic Glaze" or "Cumulus Nimbus"?: ')
     if ACCENT == 'Ceramic Glaze':
-        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Basically White" or "White"? ')
-    elif ACCENT == 'Cumnulus Nimbus':
-        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Off-White" or "Paper White"? ')
+        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Basically White" or "White"?: ')
+    elif ACCENT == 'Cumulus Nimbus':
+        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Off-White" or "Paper White"?: ')
     else:
         print MYERROR
 elif BASE == 'Manatee':
-    ACCENT = raw_input(ACCENTQ + '"Platinum Mist" or "Spartan Sage"? ')
+    ACCENT = raw_input(ACCENTQ + '"Platinum Mist" or "Spartan Sage"?: ')
     if ACCENT == 'Platinum Mist':
-        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Bone White" or "Just White"? ')
+        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Bone White" or "Just White"?: ')
     elif ACCENT == 'Spartan Sage':
-        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Fractal White" or "Not White"? ')
+        HIGHLIGHT = raw_input(HIGHLIGHTQ + '"Fractal White" or "Not White"?: ')
     else:
         print MYERROR
 else:
