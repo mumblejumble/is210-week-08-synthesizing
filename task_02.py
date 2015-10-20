0ef9a30d44e55a0c434484b6c69393fec32a536d
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-20 00:33:11 +0000

    if not SNOOZE( if not true): print beep * 5 added

--- task_02.py
+++ task_02.py
@@ -6,4 +6,7 @@
 DAY = (raw_input('What day is it? ')).lower()[:3]
 TIME = int(raw_input('What time is it? '))
 
-SNOOZE = True if (DAY == 'sat' or DAY == 'sun') or TIME < 600 else 'Beep!' * 5
+SNOOZE = True if DAY == 'sat' or DAY == 'sun' or TIME < 600 else False
+
+if not SNOOZE:
+    print 'Beep! ' * 5
